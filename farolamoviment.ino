3db2bfc06b05c00edba7f6982e2d548e7ade042d
alumnetdr2026-source <alumnetdr.2026@gmail.com>
2026-09-08 19:27:14 +0200

    Add files via upload

---
+++ farolamoviment.ino
@@ -0,0 +1,40 @@
+int pir= 7;
+int led= 2;
+int ldr= A0;
+
+
+void setup() {
+  pinMode(pir, INPUT);
+  pinMode(led, OUTPUT);
+  Serial.begin(9600);
+}
+
+
+void loop() {
+  int llum = analogRead(ldr);
+  bool nit = (llum < 500);        
+
+
+  if (nit)
+  {
+    Serial.println("Nit");
+  }
+  else
+  {
+    Serial.println("Dia");
+  }
+
+
+  if (digitalRead(pir) == HIGH && nit)
+  {
+    digitalWrite(led, HIGH);
+    Serial.println("Encès");
+    delay(200);
+  }
+  else
+  {
+    digitalWrite(led, LOW);
+    Serial.println("Apagat");
+    delay(200);
+  }
+}
